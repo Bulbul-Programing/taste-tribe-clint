51f75f4060e09af97796f963584d9bf10d6ebdc2
Bulbul Ahammed Riad <bulbulprograming@gmail.com>
2024-11-26 23:10:06 +0600

    next js middleware done

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,49 @@
 import type { NextRequest } from "next/server";
 
 import { NextResponse } from "next/server";
+import { GetCurrentUser } from "./utils/getCurrentUser";
+import { useAppSelector } from "./redux/hooks";
+import { useCurrentToken } from "./redux/features/Auth/authSlice";
+import { verifyToken } from "./utils/veryfyToken";
 
-// This function can be marked `async` if using `await` inside
+const AuthRoutes = ['/login', '/register']
+
+type Role = keyof typeof roleBasedRoutes
+
+const roleBasedRoutes = {
+  user: [/^\/user/],
+  ADMIN: [/^\/admin/],
+};
+
 export function middleware(request: NextRequest) {
-  const {pathname} = request.nextUrl
-  console.log(pathname);
-  return NextResponse.redirect(new URL("/login", request.url));
+  const { pathname } = request.nextUrl
+  const token = request.cookies.get('refreshToken')?.value
+  let user = null
+  if (token) {
+    user = verifyToken(token)
+  }
+
+  if (!user) {
+    if (AuthRoutes.includes(pathname)) {
+      return NextResponse.next()
+    }
+    else {
+      NextResponse.redirect(new URL(`/login?redirect=${pathname}`, request.url))
+    }
+  }
+
+  if (user?.role && roleBasedRoutes[user?.role as Role]) {
+    const routes = roleBasedRoutes[user?.role as Role]
+
+    if (routes.some(route => pathname.match(route))) {
+      return NextResponse.next()
+    }
+  }
+
+  return NextResponse.redirect(new URL("/", request.url));
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/admin/:path*", "/user/:path*"],
+  matcher: ["/admin/:path*", "/user/:path*", "/login", "/response"],
 };
---
+++ src/utils/getCurrentUser.ts
@@ -0,0 +1,12 @@
+"use client"
+import { useCurrentToken } from "../redux/features/Auth/authSlice";
+import { useAppSelector } from "../redux/hooks";
+import { verifyToken } from "./veryfyToken";
+
+export const GetCurrentUser = () => {
+    const userToken = useAppSelector(useCurrentToken);
+    if (userToken) {
+        const decodedToken = verifyToken(userToken);
+        return decodedToken
+    }
+}
